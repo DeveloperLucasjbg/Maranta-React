86bdd4dce65fbc335bc04c3c8ffa1edf6c4cf22d
DeveloperLucasjbg <76766269+DeveloperLucasjbg@users.noreply.github.com>
2021-03-16 05:41:06 -0300

    favs con storage, categorias mock

--- src/context/UserContextProvider.jsx
+++ src/context/UserContextProvider.jsx
@@ -8,22 +8,31 @@ const UserContextProvider = ({ children }) => {
   const isIdIn = (id) => favs.some((e) => e.id === id);
 
   const favsToogle = (id, boolean) => {
-    if (isIdIn(id)){
-      let newFavs = favs.map((e)=>
-       e.id === id ? { ...e, id: id, state: !boolean } : e 
-       )
-       setFavs(newFavs);
-    }else{
-      setFavs([ ...favs, {id: id, state: !boolean }])
+    if (isIdIn(id)) {
+      let newFavs = favs.map((e) =>
+        e.id === id ? { ...e, id: id, state: !boolean } : e
+      );
+      setFavs(newFavs);
+    } else {
+      setFavs([...favs, { id: id, state: !boolean }]);
     }
+
+    // en que momento ejecutarlo para que este actualizado el valor
+    let FavsToStorageYUser = favs.filter((e) => e.state === true);
+    localStorage.setItem("userFavs", JSON.stringify(FavsToStorageYUser));
   };
-  
-  
+
+  const getFavs = () => {
+    let storageToFavs = JSON.parse(localStorage.getItem("userFavs"));
+    return storageToFavs;
+  };
+
   return (
     <UserContext.Provider
       value={{
         favs,
-        favsToogle
+        favsToogle,
+        getFavs,
       }}
     >
       {children}
